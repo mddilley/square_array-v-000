5f2cfe03a66488ae60bea737419f70998733931a
Mike Dilley <mike.dilley@gmail.com>
2018-10-10 14:47:17 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,9 @@
-# def square_array(array)
-#   # your code here
-#   new_array = []
-#   array.each do |number|
-#     new_array << (number * number)
-#   end
-#   return new_array
-# end
-
 def square_array(array)
-  array.collect {|num| num ** 2}
+  # your code here
+  new_array = []
+  array.each do |number|
+    new_array << (number * number)
+  end
+  return new_array
 end
+
