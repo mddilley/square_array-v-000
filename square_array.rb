4372f44eda63039d8a43bb18fe3fc68909a1eddd
Mike Dilley <mike.dilley@gmail.com>
2018-10-10 14:46:20 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,12 @@
+# def square_array(array)
+#   # your code here
+#   new_array = []
+#   array.each do |number|
+#     new_array << (number * number)
+#   end
+#   return new_array
+# end
+
 def square_array(array)
-  # your code here
-  new_array = []
-  array.each do |number|
-    new_array << (number * number)
-  end
-  return new_array
+  array.collect {|num| num ** 2}
 end
